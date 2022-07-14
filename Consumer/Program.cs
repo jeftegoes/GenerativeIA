088c6c9bc20db1c0018d18ac15215fd2650f3dce
jeftegoes <jefte.goes.dev@outlook.com>
2022-07-13 23:14:34 -0300

    First commit.

---
+++ Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "default"
+};
+
+var cancellationToken = new CancellationToken();
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe("demo_topic");
+
+    while (true)
+    {
+        var consumeResult = consumer.Consume(cancellationToken);
+        Console.WriteLine(consumeResult.Message.Value);
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092",
+    ClientId = Dns.GetHostName()
+};
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    for (int i = 0; i < 10; i++)
+    {
+        var value = $"My log message: #{i}";
+        var message = new Message<Null, string> { Value = value };
+        await producer.ProduceAsync("demo_topic", message);
+        Console.WriteLine($"Send message: {value}");
+    }
+}
